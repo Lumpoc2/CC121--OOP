8189061270e00cea837980087357a3870c778e3f
Falmark lumpoc <88761455+Lumpoc2@users.noreply.github.com>
2021-10-21 02:27:14 +0800

    Linear structures

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falmark___chapter_16
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This are the item that are available in the Shop");
+            String[] item = { "Shoes", "Pencil", "Notebook", "Cellphone", "Ruler", "Erasers","Paper" };
+            foreach(string i in item)
+            {
+                Console.WriteLine(i);
+
+            }
+            method1();
+           
+        }
+        static void method1()
+        {
+            Console.Write("how many number you want to add to cart: ");
+            customearrayList<string> shoppingList = new customearrayList<string>();
+            int shop = Convert.ToInt32(Console.ReadLine());
+            for(int a = 0; a < shop; a++)
+            {
+                Console.Write("item: " + (a + 1) + ": ");
+                string Shop = Console.ReadLine();
+                shoppingList.add(Shop);     
+            }
+
+            Console.Write("Would you like to remove a item in the cart? [Y/N]  ");
+            string me = Console.ReadLine();
+            if (me == "Y" || me == "y")
+            {
+                Console.Write("item to Remove: ");
+                string rem = Console.ReadLine();
+                shoppingList.remove(rem);
+                Console.WriteLine(rem + " has remove");
+
+                Console.Write("insert a item to cart: ");
+                string tr = Console.ReadLine();
+                shoppingList.insert(2, tr);
+                Console.WriteLine("the item that i needed ", shoppingList);
+                for (int i = 0; i < shoppingList.Count; i++)
+                {
+                    Console.WriteLine("-" + shoppingList[i]);
+
+                }
+            
+                Console.WriteLine("Position of 'Paper' = {0}", shoppingList.indexOF("Paper"));
+                Console.WriteLine("Position of 'Cellphone' = {0}",
+                shoppingList.indexOF("Cellphone"));
+                Console.WriteLine("Do we have to buy Pencil? " + shoppingList.contains("Pencil"));
+
+            }
+            else if (me == "N" || me == "n" )
+            {
+                Console.Write("insert a item to cart: ");
+                string tr = Console.ReadLine();
+                shoppingList.insert(2, tr);
+                Console.WriteLine("the item that i needed ", shoppingList);
+                for (int i = 0; i < shoppingList.Count; i++)
+                {
+                    Console.WriteLine("-" + shoppingList[i]);
+
+                }
+                Console.WriteLine("Position of 'Paper' = {0}", shoppingList.indexOF("Paper"));
+                Console.WriteLine("Position of 'Cellphone' = {0}",
+                shoppingList.indexOF("Cellphone"));
+                Console.WriteLine("Do we have to buy Pencil? " + shoppingList.contains("Pencil"));
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+        }
+    }
+}
+
+   
---
+++ index.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falmark___chapter_16
+{
+    public class customearrayList<T>
+    {
+        private T[] arr;
+        private int count;
+
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+        private const int Intial_capacity = 4;
+
+        public customearrayList(int capacity = Intial_capacity)
+        {
+            this.arr = new T[capacity];
+            this.count = 0;
+        }
+        public void add(T item)
+        {
+            GrowIFArrIsFull();
+            this.arr[this.count] = item;
+            this.count++;
+
+        }
+        public void insert(int index, T item)
+        {
+            if (index > this.count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Invalid Index: " + index);
+
+            }
+            GrowIFArrIsFull();
+            Array.Copy(this.arr, index, this.arr, index + 1, this.count - index);
+            this.arr[index] = item;
+            this.count++;
+        }
+        public void GrowIFArrIsFull()
+        {
+            if (this.count + 1 > this.arr.Length)
+            {
+                T[] extendarr = new T[this.arr.Length * 2];
+                Array.Copy(this.arr, extendarr, this.count);
+                this.arr = extendarr;
+            }
+        }
+        public void clear()
+        {
+            this.arr = new T[Intial_capacity];
+            this.count = 0;
+        }
+        public int indexOF(T item)
+        {
+            for (int i = 0; i < this.arr.Length; i++)
+            {
+                if (object.Equals(item, this.arr[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public bool contains(T item)
+        {
+            int index = indexOF(item);
+            bool found = (index != -1);
+            return found;
+        }
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("invalid index: " + index);
+
+                }
+                return this.arr[index];
+            }
+            set
+            {
+                if (index >= this.count || index < 0)
+                {
+                    throw new ArgumentOutOfRangeException("invalid index: " + index);
+                }
+                this.arr[index] = value;
+            }
+        }
+        public T RemoveAt(int index)
+        {
+            if (index >= this.count || index < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                "Invalid index: " + index);
+            }
+            T item = this.arr[index];
+            Array.Copy(this.arr, index + 1,
+             this.arr, index, this.count - index - 1);
+            this.arr[this.count - 1] = default(T);
+            this.count--;
+            return item;
+        }
+        public int remove(T item)
+        {
+            int index = indexOF(item);
+            if (index != -1)
+            {
+                this.RemoveAt(index);
+            }
+            return index;
+        }
+    }
+}
+
+
+
+    
